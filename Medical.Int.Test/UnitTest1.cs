9ef6bf1253b94c8f9603ea347872e81e294082d1
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-03 23:32:30 +0530

    Added other test project

---
+++ Medical.Int.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Medical.Int.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
