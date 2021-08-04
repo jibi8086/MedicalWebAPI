d1bde806e1222396e32770f27c8870fe5200b6a4
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-05 00:00:55 +0530

    Update MedialDataClient.cs

--- Medical.External/Helpers/MedialDataClient.cs
+++ Medical.External/Helpers/MedialDataClient.cs
@@ -9,4 +9,56 @@ namespace Medical.External.Helpers
     class MedialDataClient
     {
     }
+     public class IvoaiDataClient : IIvoaiDataClient
+    {
+        private readonly HttpClient _httpClient;
+        public IvoaiDataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            using (var response = await _httpClient.GetAsync(uri))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    throw new ExternalApiException($"ivoai api failed with statuscode : {response.StatusCode }");
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode && !string.IsNullOrEmpty(content))
+                {
+                    return JsonConvert.DeserializeObject<T>(content);
+                }
+                throw new ExternalApiException($"ivoai api failed with statuscode : {response.StatusCode }");
+
+            }
+        }
+
+        public async Task<T> PostAsync<T>(string relativeUrl, object payload)
+        {
+            using (var response = await _httpClient.PostAsync(relativeUrl, new JsonContent(payload)))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    throw new ExternalApiException($"ivoai api call failed with statuscode : {response.StatusCode}");
+                }
+
+                string content = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode && !string.IsNullOrEmpty(content))
+                {
+                    return JsonConvert.DeserializeObject<T>(content);
+                }
+
+                throw new ExternalApiException($"ivoai api call failed with statuscode : {response.StatusCode}");
+            }
+        }
+    }
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object obj) : base(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json")
+        { }
+    }
 }
