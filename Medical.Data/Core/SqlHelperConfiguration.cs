cb8a523c39a68d78868d157eccea6d4459689f2b
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-05 00:17:37 +0530

    added helper

---
+++ Medical.Data/Core/SqlHelperConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Medical.Data.Contract.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical.Data.Core
+{
+    public class SqlHelperConfiguration : ISqlHelperConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+}
