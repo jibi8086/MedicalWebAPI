19dd6bc2c10518291698cd9cab24a6fdb62349bd
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-05 00:24:47 +0530

    Update ISqlFactory.cs

--- Medical.Data.Contract/Core/ISqlFactory.cs
+++ Medical.Data.Contract/Core/ISqlFactory.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Medical.Data.Contract.Core
 {
-    public interface ISqlFactory
+ public interface ISqlFactory
     {
+        /// <summary>
+        /// Creates a new, unopened <see cref="SqlConnection"/> object for the provided connection string.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>The created <see cref="SqlConnection"/> object.</returns>
+        SqlConnection CreateConnection(
+            string connectionString);
+
+        /// <summary>
+        /// Creates a new <see cref="SqlCommand"/> object based on the specified arguments.
+        /// </summary>
+        /// <param name="query">The query to be executed.</param>
+        /// <param name="connection">The SQL connection to be used.</param>
+        /// <param name="commandType">Type of the command.</param>
+        /// <param name="parameters">The query parameters.</param>
+        /// <returns>The created <see cref="SqlCommand"/> object.</returns>
+        SqlCommand CreateCommand(
+            string query,
+            SqlConnection connection,
+            CommandType commandType,
+            IEnumerable<IDbDataParameter> parameters);
     }
 }
