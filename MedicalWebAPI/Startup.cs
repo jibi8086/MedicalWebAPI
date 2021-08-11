13167b3f048e783047c5814617d9a87985e7d664
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-12 02:57:07 +0530

    added prod

--- MedicalWebAPI/Startup.cs
+++ MedicalWebAPI/Startup.cs
@@ -1,9 +1,5 @@
-using AutoMapper;
-using Medical.Data.Contract.UserLogin;
-using Medical.Domain.Contract.UserLogin;
 using Medical.Infrastructure.Configurations;
 using Medical.Infrastructure.IOC;
-using MedicalWebAPI.ViewModels;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,22 +28,20 @@ namespace MedicalWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var mapper = GetAutoMapperConfiguration();
-            services.AddSingleton(mapper);
+
             services.AddControllers();
             services.AddDependency(Configuration);
-            services.Configure<ConnectionStringSettings>(Configuration.GetSection(""));
+            services.Configure<ConnectionStringSettings>(Configuration.GetSection("ConnectionStrings"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MedicalWebAPI", Version = "v1" });
             });
-            services.Configure<ConnectionStringSettings>(Configuration.GetSection("ConnectionStrings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() ||env.IsProduction())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
@@ -64,15 +58,6 @@ namespace MedicalWebAPI
             {
                 endpoints.MapControllers();
             });
-        }
-        private static IMapper GetAutoMapperConfiguration() {
-            var configuration = new MapperConfiguration(config => {
-                config.CreateMap<UserLoginViewModel, UserLoginDomainDto>();
-                config.CreateMap<UserLoginDomainDto, UserLoginViewModel>();
-                config.CreateMap<UserLoginData, UserLoginDomainDto>();
-                config.CreateMap<UserLoginDomainDto, UserLoginData>();
-            });
-            return configuration.CreateMapper();
         }
     }
 }
