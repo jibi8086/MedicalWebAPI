8e17f6647c590f847017b28e2801ea6f2b996874
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2021-08-15 03:54:03 +0530

    Update IApplicationLogger.cs

--- Medical.Infrastructure/Logger/Contracts/IApplicationLogger.cs
+++ Medical.Infrastructure/Logger/Contracts/IApplicationLogger.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Medical.Infrastructure.Logger.Contracts
 {
-    public class IApplicationLogger
+ public interface IApplicationLogger
     {
+        void LogInformation(string message);
+        void LogInformation(Exception ex);
+        void LogInformation(Exception ex, IDictionary properties);
+        void LogWarning(string message);
+        void LogWarning(Exception ex);
+        void LogWarning(Exception ex, IDictionary properties);
+        void LogError(string message);
+        void LogError(Exception ex);
+        void LogError(Exception ex, IDictionary properties);
+        void LogError(Exception ex, string loggerName, IDictionary properties);
+
+        NLogLoggerAdapter Error();
+        NLogLoggerAdapter Warn();
+        NLogLoggerAdapter Info();
+        NLogLoggerAdapter Debug();
+        NLogLoggerAdapter Message(string message);
+        NLogLoggerAdapter Exception(Exception ex);
+        NLogLoggerAdapter Properties(IDictionary dictionaryOfProperties);
+        NLogLoggerAdapter Property(object name, object value);
+        void Write();
     }
 }
